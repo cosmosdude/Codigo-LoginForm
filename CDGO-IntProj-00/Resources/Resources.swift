b31e0371c60193ad83f51b0f5d4068ece36b8b0d
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2022-05-21 09:06:31 +0630

    Structure Project

---
+++ CDGO-IntProj-00/Resources/Resources.swift
@@ -0,0 +1,13 @@
+//
+//  Resources.swift
+//  CDGO-IntProj-00
+//
+//  Created by Thwin Htoo Aung on 2022-05-21.
+//
+
+import Foundation
+
+/// Resource namespace
+enum Resources { }
+
+typealias R = Resources
---
+++ CDGO-IntProj-00/Resources/images/Images.swift
@@ -0,0 +1,18 @@
+//
+//  Images.swift
+//  CDGO-IntProj-00
+//
+//  Created by Thwin Htoo Aung on 2022-05-21.
+//
+
+import Foundation
+import UIKit
+
+extension R {
+    
+    /// Image resource namespace
+    enum Images {
+        
+    }
+    
+}
